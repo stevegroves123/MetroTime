7cb20c757c7cc24b07572316530c1b1c323c0775
Steven Groves <stevegroves123@gmail.com>
2020-04-26 20:20:49 +0100

    Initial Commit

---
+++ MetroTime WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MetroTime WatchKit Extension
+//
+//  Created by steve groves on 26/04/2020.
+//  Copyright © 2020 steve groves. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
