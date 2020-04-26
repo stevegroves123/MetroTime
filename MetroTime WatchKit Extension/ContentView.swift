f0a4ff39e7c27174e7498669d661dfcd941c6fed
Steven Groves <stevegroves123@gmail.com>
2020-04-26 21:03:22 +0100

    Initial commit of code

--- MetroTime WatchKit Extension/ContentView.swift
+++ MetroTime WatchKit Extension/ContentView.swift
@@ -10,7 +10,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+        HStack {
+            Text("20:36")
+                .foregroundColor(Color.white)
+                .frame(width:50, height:50)
+                .background(Color.red)
+                .padding([.top, .bottom, .trailing])
+            Text("Sun")
+                .foregroundColor(Color.white)
+                .frame(width:50, height:50)
+                .background(Color.green)
+                .padding(.trailing, -10.0)
+        }
+        .padding(-10.0)
+        HStack {
+            Text("23rd")
+                .foregroundColor(Color.white)
+                .frame(width:50, height:50)
+                .background(Color.blue)
+                .padding([.top, .bottom, .trailing])
+            Text("March")
+                .foregroundColor(Color.black)
+                .frame(width:50, height:50)
+                .background(Color.yellow)
+                .padding(.trailing, -10.0)
+            }
+
+            HStack {
+            Text("2020")
+                .foregroundColor(Color.black)
+                .frame(width:50, height:50)
+                .background(Color.orange)
+                .padding([.top, .bottom, .trailing])
+            Text("Steve")
+                .foregroundColor(Color.white)
+                .frame(width:50, height:50)
+                .background(Color.purple)
+                .padding(.trailing, -10.0)
+            }
+            .padding(-10.0)
+        }
     }
 }
 
